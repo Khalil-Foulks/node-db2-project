c0783d7f1d24771d6241e4b7f4daecb88af0ab34
Khalil <khalilfoulks@gmail.com>
2020-08-11 17:42:23 -0400

    changed data type

--- migrations/20200811160219_cars_table.js
+++ migrations/20200811160219_cars_table.js
@@ -1,13 +1,13 @@
 
 exports.up = function(knex) {
   return knex.schema.createTable('cars', tbl => {
-    tbl.increments();
+    tbl.increments('car_id');
     tbl.string("vin", 17).unique().notNullable()
-    tbl.text("make").notNullable()
+    tbl.string("make").notNullable()
     tbl.string("model").notNullable()
     tbl.integer("mileage").notNullable()
     tbl.string("transmission")
-    tbl.text("title_status")
+    tbl.string("title_status")
   })
 };
 
